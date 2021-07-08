d148bfb03d506a839c870583b5a53c73e1fbf455
otreblan <otreblain@gmail.com>
2021-07-07 20:36:13 -0500

    Add --query and --train options

--- mat_model/main.py
+++ mat_model/main.py
@@ -3,9 +3,14 @@ import argparse
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Modelos de matrículas")
+    parser.add_argument("-q", "--query")
+    parser.add_argument("-t", "--train")
 
     if len(sys.argv) < 2:
         parser.print_help()
         sys.exit(1)
 
     args = parser.parse_args()
+
+    from .models import models
+    m = models()
---
+++ mat_model/models.py
@@ -0,0 +1,7 @@
+import tensorflow
+import pandas
+
+class models(object):
+    """Model training and usage"""
+    def __init__(self):
+        print("TODO")
