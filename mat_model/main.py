f44d49c09d69eb2c1c7739e2caafe6ed8571f789
otreblan <otreblain@gmail.com>
2021-07-07 21:20:20 -0500

    Add subparsers

--- mat_model/main.py
+++ mat_model/main.py
@@ -1,10 +1,35 @@
 import sys
 import argparse
 
+def import_models():
+    from .models import models
+    return models()
+
+def train(args: argparse.Namespace) -> None:
+    m = import_models()
+    m.train(args.students, args.enrollment)
+
+def query(args: argparse.Namespace) -> None:
+    m = import_models()
+    m.query(args.course, args.students)
+
 def main() -> None:
     parser = argparse.ArgumentParser(description="Modelos de matrículas")
-    parser.add_argument("-q", "--query")
-    parser.add_argument("-t", "--train")
+    subparsers = parser.add_subparsers(required=True)
+
+    parser_train = subparsers.add_parser("train")
+
+    parser_train.add_argument("-s", "--students", required=True)
+    parser_train.add_argument("-e", "--enrollment", required=True)
+
+    parser_train.set_defaults(func=train)
+
+    parser_query = subparsers.add_parser("query")
+
+    parser_query.add_argument("-c", "--course", required=True)
+    parser_query.add_argument("-s", "--students", required=True)
+
+    parser_query.set_defaults(func=query)
 
     if len(sys.argv) < 2:
         parser.print_help()
@@ -12,5 +37,4 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    from .models import models
-    m = models()
+    args.func(args)
--- mat_model/models.py
+++ mat_model/models.py
@@ -4,4 +4,10 @@ import pandas
 class models(object):
     """Model training and usage"""
     def __init__(self):
-        print("TODO")
+        pass
+
+    def train(self, students_path: str, enrollment_path: str):
+        print(f"TODO train {students_path} {enrollment_path}")
+
+    def query(self, course_id: str, students_path: str):
+        print(f"TODO query {course_id} {students_path}")
