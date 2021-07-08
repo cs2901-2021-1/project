cbe5592a500ece2d2f5fa73a38fc48aec5d31557
otreblan <otreblain@gmail.com>
2021-07-07 20:10:41 -0500

    Add argument parsing

--- mat_model/main.py
+++ mat_model/main.py
@@ -1,2 +1,11 @@
+import sys
+import argparse
+
 def main() -> None:
-    print("Hello world")
+    parser = argparse.ArgumentParser(description="Modelos de matrículas")
+
+    if len(sys.argv) < 2:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
