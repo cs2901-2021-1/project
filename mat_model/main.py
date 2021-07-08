8bab3c3aa9492f06c6dba310bab62aed8c53f125
otreblan <otreblain@gmail.com>
2021-07-08 08:29:25 -0500

    Move options to top parser

--- mat_model/main.py
+++ mat_model/main.py
@@ -7,29 +7,25 @@ def import_models():
 
 def train(args: argparse.Namespace) -> None:
     m = import_models()
-    m.train(args.students, args.enrollment)
+    m.train(args.course, args.students)
 
-def query(args: argparse.Namespace) -> None:
+def predict(args: argparse.Namespace) -> None:
     m = import_models()
-    m.query(args.course, args.students)
+    m.predict(args.course, args.students)
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Modelos de matrículas")
+
+    parser.add_argument("-c", "--course", required=True)
+    parser.add_argument("-s", "--students", required=True)
+
     subparsers = parser.add_subparsers(required=True)
 
     parser_train = subparsers.add_parser("train")
-
-    parser_train.add_argument("-s", "--students", required=True)
-    parser_train.add_argument("-e", "--enrollment", required=True)
-
     parser_train.set_defaults(func=train)
 
-    parser_query = subparsers.add_parser("query")
-
-    parser_query.add_argument("-c", "--course", required=True)
-    parser_query.add_argument("-s", "--students", required=True)
-
-    parser_query.set_defaults(func=query)
+    parser_predict = subparsers.add_parser("predict")
+    parser_predict.set_defaults(func=predict)
 
     if len(sys.argv) < 2:
         parser.print_help()
--- mat_model/models.py
+++ mat_model/models.py
@@ -6,8 +6,8 @@ class models(object):
     def __init__(self):
         pass
 
-    def train(self, students_path: str, enrollment_path: str):
-        print(f"TODO train {students_path} {enrollment_path}")
+    def train(self, course_id: str, students_path: str):
+        print(f"TODO train {course_id} {students_path}")
 
-    def query(self, course_id: str, students_path: str):
+    def predict(self, course_id: str, students_path: str):
         print(f"TODO query {course_id} {students_path}")
