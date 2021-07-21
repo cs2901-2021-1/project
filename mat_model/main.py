37493b969ef7f2b73f0a1e5f51c930178c96a5ae
otreblan <otreblain@gmail.com>
2021-07-20 19:56:04 -0500

    Add /train and /predict

--- mat_model/main.py
+++ mat_model/main.py
@@ -1,39 +1,23 @@
 import sys
 import argparse
 
-def import_models(args: argparse.Namespace):
-    from .models import models
-    return models(args.course, args.students)
+from flask import Flask, Response, request
 
-def train(args: argparse.Namespace) -> None:
-    m = import_models(args)
-    m.train()
+app = Flask(__name__)
 
-def predict(args: argparse.Namespace) -> None:
-    m = import_models(args)
+@app.route("/train")
+def train():
+    course = request.args.get("course")
+    print(course)
 
-    print("p")
-    for p in m.predict():
-        print(p)
+    return Response(course)
+
+@app.route("/predict")
+def predict():
+    course = request.args.get("course")
+    print(course)
+
+    return Response(course)
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Modelos de matrículas")
-
-    parser.add_argument("-c", "--course", required=True)
-    parser.add_argument("-s", "--students", required=True)
-
-    subparsers = parser.add_subparsers(required=True)
-
-    parser_train = subparsers.add_parser("train")
-    parser_train.set_defaults(func=train)
-
-    parser_predict = subparsers.add_parser("predict")
-    parser_predict.set_defaults(func=predict)
-
-    if len(sys.argv) < 2:
-        parser.print_help()
-        sys.exit(1)
-
-    args = parser.parse_args()
-
-    args.func(args)
+    app.run()
