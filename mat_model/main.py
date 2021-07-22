5e3a1060effd1340c183895d286315df8c6f6a04
otreblan <otreblain@gmail.com>
2021-07-21 20:38:30 -0500

    Implement courses

--- mat_model/main.py
+++ mat_model/main.py
@@ -1,7 +1,25 @@
-from json import dumps
+from json import dumps, loads
 from flask import Flask, Response, request
+from os import getenv
+
+import cx_Oracle
 
 app = Flask(__name__)
+
+def connect():
+    dsn = cx_Oracle.makedsn(
+        getenv("HOST"),
+        getenv("PORT"),
+        service_name = getenv("SID")
+    )
+
+    return cx_Oracle.connect(
+        user     = getenv("USER"),
+        password = getenv("PASSWORD"),
+        dsn      = dsn,
+        encoding = "UTF-8"
+    )
+
 
 @app.route("/train")
 def train():
@@ -22,9 +40,25 @@ def predict():
 
 @app.route("/courses")
 def courses():
-    response = dumps({})
+    cursor = connect().cursor()
 
-    return Response(response, mimetype="application/json")
+    sql ="""
+SELECT JSON_OBJECT(
+    'cursoId'     IS ca.CODACTIVIDAD,
+    'cursoDesc'   IS ca.DESCRIPCIONCORTA,
+    'cursoCod'    IS ca.IDACTIVIDAD,
+    'areaFunId'   IS gaf.CODAREAFUNCIONAL,
+    'areaFunDesc' IS gaf.DESCRIPCORTA
+)
+FROM CONFIGURACION.CON_ACTIVIDAD ca
+INNER JOIN GENERAL.GEN_AREA_FUNCIONAL gaf ON gaf.CODAREAFUNCIONAL = ca.CODAREAFUNCIONAL AND gaf.ISDELETED = 'N'
+WHERE ca.ISDELETED = 'N'
+    """
+
+    cursor.execute(sql)
+
+    return Response(dumps([loads(row[0]) for row in cursor]),
+        mimetype="application/json")
 
 def main() -> None:
     app.run()
